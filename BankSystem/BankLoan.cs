1ce1247342172909c2f6b51b4417d39400962364
Adam46072 <adam.bartl2001@seznam.cz>
2023-11-03 15:49:40 +0100

    bank loan done

--- BankSystem/BankLoan.cs
+++ BankSystem/BankLoan.cs
@@ -8,5 +8,18 @@ namespace BankSystem
 {
     internal class BankLoan
     {
+        public int amountMoney { get; private set; }
+        public double interestRate { get; private set; }
+        public double bankRecieved { get; private set; }
+        private DateTime lengthLoan { get; set; }
+        public BankLoan(int amountMoney, double interestRate, DateTime lengthLoan)
+        {
+            this.amountMoney = amountMoney;
+            this.interestRate = interestRate;
+            this.lengthLoan = lengthLoan;
+            TimeSpan ts = lengthLoan - DateTime.Now;
+            double months = ts.TotalDays / 31;
+            this.bankRecieved = amountMoney + (amountMoney * (interestRate / 100) * months);
+        }
     }
 }
