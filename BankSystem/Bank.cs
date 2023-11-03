4a00412887f2958f30e6beda61831a2df93d0d27
Adam46072 <adam.bartl2001@seznam.cz>
2023-11-03 12:42:09 +0100

    bank is almost done withou connection

--- BankSystem/Bank.cs
+++ BankSystem/Bank.cs
@@ -8,5 +8,44 @@ namespace BankSystem
 {
     internal class Bank
     {
+        public string bankName { get; private set; }
+        public enum countryResidence {CzechRepublic, Slovakia, Germany, Poland, Hungary, Austria, Switzerland}
+        public countryResidence residence { get; private set; }
+        public string bankAdress { get; private set; }
+        public int bankCode { get; init; }
+        public Website website { get; init; }
+        private List<IAccount> accounts { get; set; }
+        public List<ATM> ATMs { get; private set; }
+        public CentralBank centralBank { get; set; }
+        private LoanCentralBank loan { get; set; }
+        public Bank(string bankName, countryResidence residence, string bankAdress, int bankCode, Website website)
+        {
+            this.bankName = bankName;
+            this.residence = residence;
+            this.bankAdress = bankAdress;
+            this.bankCode = bankCode;
+            this.website = website;
+            accounts = new List<IAccount>();
+            ATMs = new List<ATM>();
+        }
+        public void GiveLoan()
+        {
+            
+        }
+        public void CreateAccount()
+        {
+
+        }
+        public void TakeLoan()
+        {
+
+        }
+        public void NewATM()
+        {
+            ATMs.Add(new ATM());
+        }
+      
+
+
     }
 }
