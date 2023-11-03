a575aee294723bf5398cdd500e906d3304b6c5fb
Adam46072 <adam.bartl2001@seznam.cz>
2023-11-03 15:53:54 +0100

    central is saving banks and can give loan

--- BankSystem/CentralBank.cs
+++ BankSystem/CentralBank.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace BankSystem
 {
-    internal class CentralBank
+    internal class CentralBank:Bank
     {
+        public double interestRate { get; private set; }
+        public List<Bank> banks { get; private set; }
+        public CentralBank(string bankName, countryResidence residence, string bankAdress, int bankCode, Website website) : base(bankName, residence, bankAdress, bankCode, website)
+        {
+            banks = new List<Bank>();
+        }
+        public void ChangeInterestRate(double percentage)
+        {
+            interestRate = percentage;
+        }
+        public override LoanCentralBank GiveLoan(int amount, DateTime lengthLoan) => new LoanCentralBank(amount, interestRate, lengthLoan);
+
+        public void AddBank(Bank bank)
+        {
+            banks.Add(bank);
+        }
+        public void RemoveBank(Bank bank)
+        {
+            banks.Remove(bank);
+        }
     }
 }
